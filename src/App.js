6cdc06a2382c7678f10acc548f0396210b24af35
JoaoACastro <joaoaccastro@gmail.com>
2025-02-11 18:04:18 -0300

    update home route

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <div>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} /> 
         <Route path="/skills" element={<Skills />} /> 
